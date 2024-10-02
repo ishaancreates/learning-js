83b0e076969d8c3fba2868b7a6d08775c3be72d5
ishaancreates <ishaanmaybe@gmail.com>
2024-10-02 16:28:49 +0530

    basics

---
+++ 02.js
@@ -0,0 +1,20 @@
+let score = "33 "
+
+console.log(typeof score) // or typeof (score)
+
+let a1 = Number (score)
+console.log(typeof a1)
+console.log(a1)
+
+
+// "33"=> 33
+// "33abc" => NaN
+// true => 1; false => 0
+ let isLoggedIn = ""
+
+ let booleanIsLoggedIn = Boolean (isLoggedIn)
+ console.log (booleanIsLoggedIn)
+
+ // "anything" =. true 
+
+ // String(),used to convert to string 
--- text.js
+++ text.js
@@ -1,4 +1,13 @@
-console.log("Hitesh chaudhary")
+const accountId =21
+let accountEmail = "IShaan@google.com"
+var accountPass = "123"
+accountCity = "Sultanpur"
+console.log(accountCity);
+console.log("hi");
+accountEmail="njqdn@mail.com"
+accountPass= "795"
+accountCity= "Lucknow"
+let accountState /* undefined likh k aayega */
+console.table([accountId, accountEmail, accountPass, accountState])
 
-
-hgjhggjk
+/* dont use var scope wali dikkat */ 
